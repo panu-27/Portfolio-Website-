666e46eea8a28e73066b417f8170a4346fa0d959
panu-27 <pranavzinjad06@gmail.com>
2024-10-14 22:26:09 +0530

    error in hero fixed

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
         </motion.div>
 
         <p className="text-center text-xl max-w-[500px] mx-auto mt-8 text-white/70">
-        "Hi, I’m Pranav, a Computer Science student at NBNSTIC, affiliated with SPPU. I specialize in MERN stack development, crafting dynamic web applications that enhance user e
+        "Hi, I'm Pranav, a Computer Science student at NBNSTIC, affiliated with SPPU. I specialize in MERN stack development, crafting dynamic web applications that enhance user experince"
         </p>
 
         <Image
